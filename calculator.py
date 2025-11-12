8b4109df2f63b94f721f4474269ef48b01b65136
AlexTungol <alexander.tungol@ufl.edu>
2025-11-12 13:12:24 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -5,3 +5,22 @@ calculator.py
 One function per operation, in order.
 """
 import math
+def add(a,b):
+    a+b
+def sub(a,b):
+    a-b
+def mul(a,b):
+    a*b
+def div(a,b):
+    try:
+        a/b
+    except ValueError:
+        print("Cannont divide by zero")
+def log(a,b):
+    try:
+        math.log(a,b)
+    except ValueError:
+        print("Can't log number less than or equal to zero")
+
+def exp(a,b):
+    a**b
