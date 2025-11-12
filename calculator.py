781b7c7ec4de0deaec4b4c8f60c756d569bb9ee9
Stefan-Lotz <Stefanlotz07@gmail.com>
2025-11-12 12:52:24 -0500

    modified calculator.py

--- calculator.py
+++ calculator.py
@@ -5,3 +5,27 @@ calculator.py
 One function per operation, in order.
 """
 import math
+
+def add(a, b): 
+    return a + b
+
+def sub(a, b): 
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    try:
+        return b / a
+    except ZeroDivisionError:
+        print("Cannot divide by zero!")
+
+def log(a, b):
+    try:
+        return math.log(b, a)
+    except ValueError:
+        print("Cannot take the log of a number less than or equal to zero!")
+
+def exp(a, b):
+    return a ** b
